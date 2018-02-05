3bcf9f7ff0f11f4afcf8057208c90da7e8b755fb
slimpotatoes <slimpotatoes@bitbucket.org>
2018-02-05 16:13:22 -0500

    colorbar added

--- imagedisplay.py
+++ imagedisplay.py
@@ -3,10 +3,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.widgets import Button, SpanSelector
-from matplotlib_scalebar.scalebar import ScaleBar
+# from matplotlib_scalebar.scalebar import ScaleBar
+from matplotlib.colorbar import Colorbar
 import numpy as np
-import time
-import string
+# import time
+# import string
 import collections
 
 
@@ -51,21 +52,29 @@ class ImageDisplay(object):
             self.fig_image_parameter.append(button)
 
         # Define image axis
-        self.ax_image = plt.subplot(self.gs_fig_image[1:-1, 1:-1])
+        self.ax_image = plt.subplot(self.gs_fig_image[1:-1, 1:6])
         self.ax_image.set_axis_off()
-        self.image = self.ax_image.imshow(self.image_data, cmap='gray')
+        self.cmap = 'gray'
+        self.image = self.ax_image.imshow(self.image_data, cmap=self.cmap)
 
         # Contrast histogram display and span selector
-        self.ax_contrast = plt.subplot(self.gs_fig_image[0, 1:-1])
+        self.ax_contrast = plt.subplot(self.gs_fig_image[0, 1:6])
         self.contrastbins = 256
         self.cmin = np.min(self.image_data)
         self.cmax = np.max(self.image_data)
         self.imhist, self.imbins = np.histogram(self.image_data, bins=self.contrastbins)
+        self.ax_contrast_span = None
         self.plot_contrast_histogram()
 
         # Colormaps
         self.maps = sorted([m for m in plt.cm.datad if not m.endswith("_r")])
+        self.cmapfig = None
+        self.cmapaxes = None
         # (https://scipy.github.io/old-wiki/pages/Cookbook/Matplotlib/Show_colormaps)
+
+        # Colourbar
+        self.ax_colourbar = plt.subplot(self.gs_fig_image[1:-1, 7])
+        self.colourbar = Colorbar(self.ax_colourbar, self.image)
 
         # Show the display window
         plt.show()
@@ -76,44 +85,51 @@ class ImageDisplay(object):
 
     # Button to open colourmap selection window
     def colourmap_button(self, event):
-        nummaps = len(self.maps)
-        self.cmapfig = plt.figure('Colourmap options, pick one!', figsize=(5, 2 * nummaps))
-        self.cmapaxes = {}
-        gradient = np.linspace(0, 1, 100) * np.ones((3, 100))
+        if event.inaxes == self.fig_image_parameter[1].ax:
+            nummaps = len(self.maps)
+            self.cmapfig = plt.figure('Colourmap options, pick one!', figsize=(5, 2 * nummaps))
+            self.cmapaxes = {}
+            gradient = np.linspace(0, 1, 100) * np.ones((3, 100))
 
-        for mm in range(nummaps):
-            corners = [0., mm / float(nummaps), 0.75, 1. / nummaps]
-            self.cmapaxes[mm] = plt.axes(corners)
-            self.cmapaxes[mm].annotate(self.maps[mm], xy=(0.77, (mm + 0.2) / float(nummaps)),
-                                       xycoords='figure fraction', fontsize=11)
-            self.cmapaxes[mm].set_axis_off()
-            self.cmapaxes[mm].imshow(gradient, cmap=plt.get_cmap(self.maps[mm]))
+            for mm in range(nummaps):
+                corners = [0., mm / float(nummaps), 0.75, 1. / nummaps]
+                self.cmapaxes[mm] = plt.axes(corners)
+                self.cmapaxes[mm].annotate(self.maps[mm], xy=(0.77, (mm + 0.2) / float(nummaps)),
+                                           xycoords='figure fraction', fontsize=11)
+                self.cmapaxes[mm].set_axis_off()
+                self.cmapaxes[mm].imshow(gradient, cmap=plt.get_cmap(self.maps[mm]))
 
-        self.cmapfig.canvas.mpl_connect('button_press_event', self.colourmap_axis_select)
+            self.cmapfig.canvas.mpl_connect('button_press_event', self.colourmap_axis_select)
 
-        plt.show()
+            plt.show()
 
     # Set colourmap based on clicking on an axis in the colourmap window
     def colourmap_axis_select(self, event):
         for aa in self.cmapaxes:
             if event.inaxes == self.cmapaxes[aa]:
-                self.image.set_cmap(plt.get_cmap(self.maps[aa]))
+                self.cmap = self.maps[aa]
+                self.image.set_cmap(plt.get_cmap(self.cmap))
+                self.update_colourmap()
                 self.fig_image.canvas.draw()
 
     # Function to update image after changing it
     def update_image(self):
         self.image.set_clim(vmin=self.cmin, vmax=self.cmax)
 
+    def update_colourmap(self):
+        self.colourbar.update_bruteforce(self.image)
+
     # Calculates and plots image histogram and connects interactive spanselector
     def plot_contrast_histogram(self):
         self.ax_contrast.cla()
         self.ax_contrast.plot(self.imbins[:-1], self.imhist, color='k')
         self.ax_contrast.set_axis_off()
-        self.contrast_span = SpanSelector(self.ax_contrast, self.contrast_span, 'horizontal',
-                                          span_stays=True, rectprops=dict(alpha=0.5, facecolor='green'))
+        self.ax_contrast_span = SpanSelector(self.ax_contrast, self.contrast_span, 'horizontal',
+                                             span_stays=True, rectprops=dict(alpha=0.5, facecolor='green'))
 
     # Function for interactive spanselector for contrast histogram
     def contrast_span(self, cmin, cmax):
         self.cmin = cmin
         self.cmax = cmax
         self.update_image()
+        self.update_colourmap()
